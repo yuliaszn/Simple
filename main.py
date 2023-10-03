dae40d8d86b65099c762f3015c33de290a60931a
user <julia.sazanovich.11@gmail.com>
2023-10-03 20:55:27 +0300

    lovebaby

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df_inifian = pd.read_csv(r'kit/team.csv')
+
+print(df_inifian, ['data'])
+
+fig,ax = plt.subplots( figsize = (10 , 6))
+ax.plot(df_inifian['Data'], df_inifian['Time'], '-')
+plt.legend(["This is my legend"], fontsize="x-large")
+plt.show()
